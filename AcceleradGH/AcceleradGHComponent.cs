7e46416dd2978d265e240c6ffc344d53a53990c0
Nathaniel Jones <nljones@mit.edu>
2016-10-26 10:17:50 -0400

    First commit

---
+++ AcceleradGH/AcceleradGHComponent.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+using Rhino;
+using Rhino.Commands;
+using Rhino.Display;
+using Rhino.DocObjects;
+
+namespace AcceleradGH
+{
+    public class AcceleradGHComponent : GH_Component
+    {
+        /// <summary>
+        /// Last wake state.
+        /// </summary>
+        protected bool state = false;
+
+        /// <summary>
+        /// A change has been made to the model during the current command.
+        /// </summary>
+        protected bool modelChanged = false;
+
+        /// <summary>
+        /// A change occurred during a completed command.
+        /// </summary>
+        protected bool doUpdate = false;
+
+        /// <summary>
+        /// Timer is running.
+        /// </summary>
+        protected bool timerRunning = false;
+
+        protected Timer trampoline = new Timer();
+
+        protected GH_Document ghDocument = null;
+
+        /// <summary>
+        /// Last sun direction.
+        /// </summary>
+        protected Vector3d solar = new Vector3d(0, 0, 0);
+
+        protected int count = 0;
+
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public AcceleradGHComponent()
+            : base("Watch for Change", "Watch",
+                "Signal when the Rhino model has changed.",
+                "Accelerad", "RT")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Awake", "A", "AcceleradRT is listening for changes", GH_ParamAccess.item, false);
+            pManager.AddVectorParameter("Sun Vector", "S", "Direction to sun", GH_ParamAccess.item, solar);
+            pManager.AddIntegerParameter("Interval", "I", "Time interval between checks (milliseconds)", GH_ParamAccess.item, 1000);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("Update", "U", "Send update to AcceleradRT", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Count", "C", "Times run", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // Use the DA object to retrieve the data inside the first input parameter.
+            // If the retrieval fails (for example if there is no data) we need to abort.
+            bool awake = false;
+            DA.GetData(0, ref awake);
+
+            Vector3d sun = new Vector3d(solar);
+            DA.GetData(1, ref sun);
+            bool timeChanged = !solar.Equals(sun);
+            if (timeChanged)
+                solar = sun;
+
+            int interval = 1000;
+            DA.GetData(2, ref interval);
+            trampoline.Interval = interval;
+
+            bool update = false;
+
+            if (awake)
+            {
+                if (ghDocument == null)
+                    ghDocument = this.OnPingDocument();
+                ghDocument.SolutionEnd += documentSolutionEnd;
+
+                if (!state)
+                {
+                    // Register command event handlers
+                    RhinoDoc.AddRhinoObject += HandleAddObject;
+                    RhinoDoc.DeleteRhinoObject += HandleDeleteObject;
+                    RhinoDoc.UndeleteRhinoObject += HandleUndeleteObject;
+                    //RhinoDoc.PurgeRhinoObject += HandlePurgeObject;
+                    //RhinoDoc.ReplaceRhinoObject += HandleReplaceObject;
+                    //RhinoDoc.ModifyObjectAttributes += HandleModifyObject;
+                    //RhinoDoc.MaterialTableEvent += HandleMaterial;
+
+                    Command.BeginCommand += HandleBeginCommand;
+                    Command.EndCommand += HandleEndCommand;
+                    //Command.UndoRedo += HandleUndoRedo;
+
+                    update = true;
+                }
+                else
+                {
+                    update = doUpdate || timeChanged;
+                }
+            }
+            else if (state)
+            {
+                // Register command event handlers
+                RhinoDoc.AddRhinoObject -= HandleAddObject;
+                RhinoDoc.DeleteRhinoObject -= HandleDeleteObject;
+                RhinoDoc.UndeleteRhinoObject -= HandleUndeleteObject;
+                //RhinoDoc.PurgeRhinoObject -= HandlePurgeObject;
+                //RhinoDoc.ReplaceRhinoObject -= HandleReplaceObject;
+                //RhinoDoc.ModifyObjectAttributes -= HandleModifyObject;
+                //RhinoDoc.MaterialTableEvent -= HandleMaterial;
+
+                Command.BeginCommand -= HandleBeginCommand;
+                Command.EndCommand -= HandleEndCommand;
+                //Command.UndoRedo -= HandleUndoRedo;
+            }
+            state = awake;
+            doUpdate = false;
+
+            DA.SetData(0, update);
+            DA.SetData(1, count++);
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                return Properties.Resources.vfd;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{23ed7027-4273-4cee-acb6-9c97be1f9d05}"); }
+        }
+
+        #region Command Event Handlers
+
+        public void HandleAddObject(Object sender, RhinoObjectEventArgs e)
+        {
+            //if (!modelOpening)
+            //{
+                modelChanged = true;
+            //    Print("Add " + e.TheObject.ObjectType.ToString());
+            //}
+        }
+
+        public void HandleDeleteObject(Object sender, RhinoObjectEventArgs e)
+        {
+            modelChanged = true;
+            //Print("Delete " + e.TheObject.ObjectType.ToString());
+        }
+
+        public void HandleUndeleteObject(Object sender, RhinoObjectEventArgs e) // Similar to add
+        {
+            modelChanged = true;
+            //Print("Undelete " + e.TheObject.ObjectType.ToString());
+        }
+
+        //public void HandlePurgeObject(Object sender, RhinoObjectEventArgs e)
+        //{
+        //    modelChanged = true;
+        //    Print("Purge Object " + e.TheObject.Id);
+        //}
+
+        //public void HandleReplaceObject(Object sender, RhinoReplaceObjectEventArgs e)
+        //{
+        //    modelChanged = true;
+        //    Print("Replace Object " + e.OldRhinoObject.Id + " with " + e.NewRhinoObject.Id);
+        //}
+
+        //public void HandleModifyObject(Object sender, RhinoModifyObjectAttributesEventArgs e)
+        //{
+        //    modelChanged = true;
+        //    Print("Modify Object " + e.RhinoObject.Id);
+        //}
+
+        //public void HandleMaterial(Object sender, Rhino.DocObjects.Tables.MaterialTableEventArgs e)
+        //{
+        //    modelChanged = true;
+        //    Print("Material " + e.Index);
+        //}
+
+        public void HandleBeginCommand(Object sender, CommandEventArgs e)
+        {
+            //EndIdleTime();
+            ////PrintDebug("Begin Command " + e.CommandEnglishName);
+        }
+
+        public void HandleEndCommand(Object sender, CommandEventArgs e)
+        {
+            if (e.CommandResult == Result.Success && modelChanged)
+            {
+                //AcceptIdleTime();
+                modelChanged = false;
+                doUpdate = true;
+                //idleTime.Restart(); // Restart from zero
+            }
+            //Print("End Command " + e.CommandEnglishName + ": " + e.CommandResult.ToString());
+        }
+
+        /// <summary>
+        /// Based on trampoline.ghx: http://www.grasshopper3d.com/forum/topics/timers-and-theengine-script?commentId=2985220%3AComment%3A129569
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void documentSolutionEnd(object sender, GH_SolutionEventArgs e)
+        {
+            ghDocument.SolutionEnd -= documentSolutionEnd;
+
+            // The trampoline gives time to stop the infinitly recursive chain from canvas,
+            // as well as mantains the call stack fixed in length
+            if (!timerRunning)
+            {
+                timerRunning = true;
+                trampoline.Tick += trampolineTick;
+                trampoline.Start();
+            }
+        }
+
+        public void trampolineTick(object sender, EventArgs e)
+        {
+            trampoline.Tick -= trampolineTick;
+            trampoline.Stop();
+            timerRunning = false;
+            this.ExpireSolution(true);
+        }
+
+        #endregion
+    }
+}
---
+++ AcceleradGH/AcceleradGHInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace AcceleradGH
+{
+    public class AcceleradGHInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "AcceleradGH";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return Properties.Resources.vfd;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "Components for Accelerad";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("ea9f0eaf-4a68-44c7-a22c-7b8cf006f931");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "Nathaniel Jones";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "nljones@mit.edu";
+            }
+        }
+    }
+}
